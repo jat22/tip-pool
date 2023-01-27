1d2275506dc2384f4ed82ecdd6ecbb0087e37110
John Wood <johna613@gmail.com>
2023-01-27 10:04:35 -0500

    update

--- servers.test.js
+++ servers.test.js
@@ -1,31 +1,31 @@
-// describe("Servers test (with setup and tear-down)", function() {
-//   beforeEach(function () {
-//     // initialization logic
-//     serverNameInput.value = 'Alice';
-//   });
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
 
-//   it('should add a new server to allServers on submitServerInfo()', function () {
-//     submitServerInfo();
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
 
-//     expect(Object.keys(allServers).length).toEqual(1);
-//     expect(allServers['server' + serverId].serverName).toEqual('Alice');
-//   });
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
 
-//   it('should update server table', function(){
-//     submitServerInfo();
-//     updateServerTable();
+  it('should update server table', function(){
+    submitServerInfo();
+    updateServerTable();
     
-//     let updatedTable = document.querySelectorAll('#serverTable tbody tr td');
+    let updatedTable = document.querySelectorAll('#serverTable tbody tr td');
 
-//     expect(updatedTable.length).toEqual(2);
-//     expect(updatedTable[0]).toEqual('Alice');
-//     expect(updatedTable[1]).toEqual('$0.00');
+    expect(updatedTable.length).toEqual(2);
+    expect(updatedTable[0]).toEqual('Alice');
+    expect(updatedTable[1]).toEqual('$0.00');
  
-//   })
+  })
 
-//   afterEach(function() {
-//     serverTbody.innerHTML = '';
-//     serverId = 0;
-//     allServers = {};
-//   });
-// });
+  afterEach(function() {
+    serverTbody.innerHTML = '';
+    serverId = 0;
+    allServers = {};
+  });
+});
