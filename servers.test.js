44dd854c2229446160898c089eb78b53f6b8ac02
John Wood <johna613@gmail.com>
2023-01-27 09:07:00 -0500

    adding files

---
+++ servers.test.js
@@ -0,0 +1,31 @@
+// describe("Servers test (with setup and tear-down)", function() {
+//   beforeEach(function () {
+//     // initialization logic
+//     serverNameInput.value = 'Alice';
+//   });
+
+//   it('should add a new server to allServers on submitServerInfo()', function () {
+//     submitServerInfo();
+
+//     expect(Object.keys(allServers).length).toEqual(1);
+//     expect(allServers['server' + serverId].serverName).toEqual('Alice');
+//   });
+
+//   it('should update server table', function(){
+//     submitServerInfo();
+//     updateServerTable();
+    
+//     let updatedTable = document.querySelectorAll('#serverTable tbody tr td');
+
+//     expect(updatedTable.length).toEqual(2);
+//     expect(updatedTable[0]).toEqual('Alice');
+//     expect(updatedTable[1]).toEqual('$0.00');
+ 
+//   })
+
+//   afterEach(function() {
+//     serverTbody.innerHTML = '';
+//     serverId = 0;
+//     allServers = {};
+//   });
+// });
